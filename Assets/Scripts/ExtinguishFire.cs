29f4067fa36ea93ea36775055909fc79fc59f95e
lucasnaka <lunetonaka@gmail.com>
2019-11-25 19:31:13 -0300

    Feature: network animator player adicionado

--- Assets/Scripts/ExtinguishFire.cs
+++ Assets/Scripts/ExtinguishFire.cs
@@ -11,29 +11,29 @@ public class ExtinguishFire : MonoBehaviour
 
     void ApagarFogo()
     {
-        if(faseDoFogo == 4)
-        {
+        //if(faseDoFogo == 4)
+        //{
             var alpha_main = alpha.main;
             alpha_main.loop = false;
-        }
+        //}
         
-        if(faseDoFogo == 3)
-        {
+        //if(faseDoFogo == 3)
+        //{
             var add_main = add.main;
             add_main.loop = false;
-        }
+        //}
         
-        if(faseDoFogo == 2)
-        {
+        //if(faseDoFogo == 2)
+        //{
             var glow_main = glow.main;
             glow_main.loop = false;
-        }
+        //}
         
-        if(faseDoFogo == 1)
-        {
+        //if(faseDoFogo == 1)
+        //{
             var sparks_main = sparks.main;
             sparks_main.loop = false;
-        }
+        //}
     }
 
     int reachRange = 100;
@@ -41,15 +41,16 @@ public class ExtinguishFire : MonoBehaviour
 
     void Update()
     {
-        var waitTime = 5.0;
-        var charge = 0.0;
+        //var waitTime = 5.0;
+        //var charge = 0.0;
 
         if(Input.GetMouseButtonDown(0))
         {
-            charge += Time.deltaTime;
+            CheckHitObj();
+            //charge += Time.deltaTime;
         }
 
-        if(Input.GetMouseButtonUp(0))
+        /* if(Input.GetMouseButtonUp(0))
         {
             charge = 0.0;
         }
@@ -59,7 +60,7 @@ public class ExtinguishFire : MonoBehaviour
             Debug.Log("Apagando o fogo");
             charge = 0.0;
             faseDoFogo++;
-        }
+        } */
     }
 
     void CheckHitObj()
